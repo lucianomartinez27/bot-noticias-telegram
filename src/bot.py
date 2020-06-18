c4b2725664a6b05508d814d6c314189e697b8a9c
Luciano Martínez <lucianomartinez27@gmail.com>
2020-06-18 17:23:50 -0300

    se añadieron nuevas funciones y comandos

--- src/bot.py
+++ src/bot.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
+# Telegram library
 from telegram.ext import Updater
-from config.token import token
+from config.keys import TOKEN, APIKEY
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 import logging
+
+# Import NEWSAPI
+from newsapi import NewsApiClient
+
+# Init NEWSAPI
+newsapi = NewsApiClient(api_key=APIKEY)
+
 
 class Bot:
     """ Crea un objeto Bot para telegram.
@@ -15,12 +23,13 @@ class Bot:
 
     """
     def __init__(self, nombre):
+        """Inicializa las variables básicas para que un bot de Telegram funcione"""
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
         self.logger = logging.getLogger(nombre)
         self.level = logging.INFO
 
         # Updater: es el encargado de contestar a los comandos que envíe el usuario.
-        self.updater = Updater(token=token, use_context=True)
+        self.updater = Updater(token=TOKEN, use_context=True)
         # Está a la espera de que se ingresen comandos
         self.updater.start_polling()
         # Dispatcher: está al pendiente de todas las ventanas donde se encuentra el bot.
@@ -36,7 +45,7 @@ class Bot:
     
     def esperar_mensaje(self):
         """ Espera cualquier cosa en el chat que no sea un comando """ 
-        mensaje = MessageHandler(Filters.text & (~Filters.command), self.echo)
+        mensaje = MessageHandler(Filters.text & (~Filters.command), self.contestar_mensaje)
         self.dispatcher.add_handler(mensaje)
 
     def contestar_mensaje(self, update, context):
@@ -61,10 +70,29 @@ class Bot:
         context.bot.send_message(
             chat_id = update.message.chat_id,
             text = """Los comandos que pueden ayudarte son:
-            /secciones
-            /diarios
-            /clima
+        /seccion - "Busca las noticias por sección"
+        /clima - "Te proporcina los dos climáticos en argentina"
+        /top5 - "Devuelve las 5 noticias más relevantes del momento"
             """)
+    def top_noticias(self, update, context):
+        """ Devuelve las 5 noticias más importantes del momento en Argentina"""
+        self.logger.info('He recibido el comando top5')
+        top5_noticias = newsapi.get_top_headlines(
+                                          language='es',
+                                          country='ar',
+                                          page_size=5)
+        for articulo in top5_noticias['articles']:
+                context.bot.send_message(
+                chat_id = update.message.chat_id,
+                text = "{} - {} \n {}".format(articulo['title'], articulo['author'], articulo['description']))
+                context.bot.send_message(
+                    chat_id = update.message.chat_id,
+                    text = "{}".format(articulo['description']))
+
+    #def noticia_por_tema(self, tema):
+        #"""Devuelve las 5 noticias más relevantes para el tema que se elija"""
+        
+
 
 
 
@@ -72,4 +100,5 @@ if __name__ == '__main__':
     noticias_bot = Bot("noticias_argentina_bot")
     noticias_bot.esperar_comando("start", noticias_bot.start)
     noticias_bot.esperar_comando("ayuda", noticias_bot.ayuda)
+    noticias_bot.esperar_comando("top5", noticias_bot.top_noticias)
     noticias_bot.esperar_mensaje()
